dfc613ce94626831651879cf08c66747ce11db46
jonsalazar09 <jonsalazar09@gmail.com>
2024-09-04 21:59:21 +0800

    Update Statistics.styled.jsx

--- src/components/Statistics/Statistics.styled.jsx
+++ src/components/Statistics/Statistics.styled.jsx
@@ -1,36 +1,24 @@
 import styled from 'styled-components';
 
-const SectionWrapper = styled.section`
-  padding: 30px 0;
+const StatList = styled.ul`
+  padding-bottom: 30px;
+  padding-left: 30px;
+
+  font-size: 18px;
+  color: #768696;
 `;
 
-const Container = styled.div`
-  width: 100%;
-
-  margin: 0 auto;
-  padding: 0 15px;
-
-  max-width: 480px;
+const StatListItem = styled.li`
+  :not(:last-child) {
+    margin-bottom: 20px;
+  }
 `;
 
-const Wrapper = styled.div`
-  border: 1px solid #e5eaf1;
-  border-radius: 8px;
-  overflow: hidden;
+const Span = styled.span`
+  margin-left: 10px;
 
-  background-color: #f3f6f9;
-  box-shadow: 4px 1px 4px rgba(0, 0, 0, 0.12), 0 4px 4px rgba(0, 0, 0, 0.06),
-    1px 4px 6px rgba(0, 0, 0, 0.16);
-`;
-
-const Title = styled.h2`
-  padding: 30px;
-
-  text-align: center;
-
-  font-size: 26px;
-  font-weight: bold;
+  font-weight: 600;
   color: #112236;
 `;
 
-export { SectionWrapper, Container, Wrapper, Title };
+export { StatList, StatListItem, Span };
